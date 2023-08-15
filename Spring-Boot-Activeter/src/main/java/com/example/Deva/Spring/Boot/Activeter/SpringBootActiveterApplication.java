dc7ad1a617ea81b6402c8b7c805415b5c9d61eb1
PeddolluDeva <peddolludeva803@gmail.com>
2023-08-15 15:10:46 +0530

    Sample of Activeter

---
+++ Spring-Boot-Activeter/src/main/java/com/example/Deva/Spring/Boot/Activeter/SpringBootActiveterApplication.java
@@ -0,0 +1,13 @@
+package com.example.Deva.Spring.Boot.Activeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootActiveterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootActiveterApplication.class, args);
+		System.out.println("Deva can be stated activeter");
+	}
+}
---
+++ Spring-Boot-Activeter/src/test/java/com/example/Deva/Spring/Boot/Activeter/SpringBootActiveterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Deva.Spring.Boot.Activeter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootActiveterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
